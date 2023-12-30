5364afc323b46472b627a6060a967b2ce3ac077d
Arabinda MOHAPATRA <53084042+ARBINDA765@users.noreply.github.com>
2023-12-30 17:11:02 +0530

    Add files via upload
    SQL Query to identify the consecutive numbers in the table:Asked by Amazon

---
+++ Consutive Numbers Appear AMAZAN SQL Intreview.sql
@@ -0,0 +1,46 @@
+
+--Write a SQL Query to identify the consecutive numbers in the table:Asked by Amazan
+
+-------------------------------------Sample Table----------------------
+
+USE SSIS
+CREATE TABLE logs 
+(
+    ID	INT,
+    Value	INT
+);
+
+INSERT INTO logs (ID, Value) VALUES ('1', '10');
+INSERT INTO logs (ID, Value) VALUES ('2', '12');
+INSERT INTO logs (ID, Value) VALUES ('3', '12');
+INSERT INTO logs (ID, Value) VALUES ('4', '12');
+INSERT INTO logs (ID, Value) VALUES ('5', '10');
+INSERT INTO logs (ID, Value) VALUES ('6', '10');
+-------------end of Table---------------------------------------------------
+SELECT * FROM logs
+------------------------Solution 1--------------------------
+SELECT Value FROM
+(
+SELECT ID,Value,LEAD(Value,1) OVER(ORDER BY ID)as 'Next_Cell',LEAD(Value,2) OVER(ORDER BY ID)as 'Next_to_Next_Cell'
+FROM logs
+) A
+WHERE Value=Next_Cell AND Next_Cell = Next_To_Next_Cell
+
+------------------------------Solution 2--------------------------------------------
+SELECT  L1.ID,L2.ID,L3.ID,L3.Value as 'Consutive Number'FROM logs L1
+
+--INNER JOIN logs L2 ON L1.ID=L2.ID-1 AND L1.Value=L2.Value
+--INNER JOIN logs L3 ON L2.ID=L3.ID-1 AND L2.Value=L3.Value
+
+INNER JOIN logs L2 ON L1.ID=L2.ID+1 AND L1.Value=L2.Value
+INNER JOIN logs L3 ON L2.ID=L3.ID+1 AND L2.Value=L3.Value
+
+
+
+
+
+
+
+
+
+

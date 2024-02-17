4b4c76e90a1d81d4ad5c49985ac658741aac5681
Arabinda MOHAPATRA <53084042+ARBINDA765@users.noreply.github.com>
2024-02-17 08:20:41 +0530

    Add files via upload
    Consutive Numbers Appear AMAZAN SQL Intreview.sql
    3 Solution provided

--- Consutive Numbers Appear AMAZAN SQL Intreview.sql
+++ Consutive Numbers Appear AMAZAN SQL Intreview.sql
@@ -1,5 +1,5 @@
 
---Write a SQL Query to identify the consecutive numbers in the table:Asked by Amazan
+--Write a SQL Query to identify the consecutive numbers in the table:Asked by amazon
 
 -------------------------------------Sample Table----------------------
 
@@ -19,7 +19,7 @@ INSERT INTO logs (ID, Value) VALUES ('6', '10');
 -------------end of Table---------------------------------------------------
 SELECT * FROM logs
 ------------------------Solution 1--------------------------
-SELECT Value FROM
+SELECT A.ID,A.Value FROM
 (
 SELECT ID,Value,LEAD(Value,1) OVER(ORDER BY ID)as 'Next_Cell',LEAD(Value,2) OVER(ORDER BY ID)as 'Next_to_Next_Cell'
 FROM logs
@@ -35,10 +35,27 @@ SELECT  L1.ID,L2.ID,L3.ID,L3.Value as 'Consutive Number'FROM logs L1
 INNER JOIN logs L2 ON L1.ID=L2.ID+1 AND L1.Value=L2.Value
 INNER JOIN logs L3 ON L2.ID=L3.ID+1 AND L2.Value=L3.Value
 
+------------------------------Solution 3--------------------------------------------
 
+SELECT A.ID,A.Value FROM
+(
+SELECT ID,Value,LEAD(Value,1) OVER(ORDER BY ID)as 'Next_Cell',LAG(Value,1) OVER(ORDER BY ID)as 'Previous_Cell'
+FROM logs
+) A
+WHERE A.Value=A.Next_Cell
+GROUP BY A.Value
+HAVING COUNT(*)>1
 
+UNION
 
-
+SELECT A.Value FROM
+(
+SELECT ID,Value,LEAD(Value,1) OVER(ORDER BY ID)as 'Next_Cell',LAG(Value,1) OVER(ORDER BY ID)as 'Previous_Cell'
+FROM logs
+) A
+WHERE Value=A.Previous_Cell
+GROUP BY A.Value
+HAVING COUNT(*) > 1
 
 
 

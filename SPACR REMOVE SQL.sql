9c07340b100c833d12031cc3e4ac819f1c8941e4
Arabinda MOHAPATRA <53084042+ARBINDA765@users.noreply.github.com>
2024-03-10 13:39:25 +0530

    Add files via upload
    -Question - Segregate the name into first name and last name and keep them in correct order of caps.

---
+++ SPACR REMOVE SQL.sql
@@ -0,0 +1,29 @@
+--Question - Segregate the name into first name and last name and keep them in correct order of caps. 
+--Table Creation Query :-
+USE SSIS
+create table names_t( 
+name varchar(30));
+
+insert into names_t
+values('aNkiT DhAR'),
+('RahUL BhaTIYa');
+
+--------
+
+SELECT * FROM names_t
+
+
+WITH CTE_A as
+(
+SELECT * 
+,CHARINDEX(' ',name) 'Space_Index',SUBSTRING(name,1,CHARINDEX(' ',name)) 'Fname',
+SUBSTRING(name,CHARINDEX(' ',name)+1,LEN(name)) as 'Lname'
+
+FROM names_t
+)
+
+SELECT CONCAT(UPPER(SUBSTRING(Fname,1,1)),LOWER(SUBSTRING(Fname,2,LEN(Fname)))) AS 'First_Name',
+ CONCAT(UPPER(SUBSTRING(Lname,1,1)),LOWER(SUBSTRING(Lname,2,LEN(Fname)))) AS 'Last_Name'
+
+
+FROM CTE_A

4f2b138649a02337d461368d293fa381d7e8f6d2
Arabinda MOHAPATRA <53084042+ARBINDA765@users.noreply.github.com>
2024-03-26 18:29:34 +0530

    Add files via upload
    Return Orders Customer Feedback

---
+++ Return Orders Customer Feedback.sql
@@ -0,0 +1,65 @@
+USE SSIS
+
+----Return Orders Customer Feedback--
+
+--------------------Sample Input---------------------------------------------
+
+DROP TABLE Flipkart_tb
+CREATE TABLE Flipkart_tb 
+(
+    order_id	INT,
+    order_date	DATE,
+    customer_name	VARCHAR(230),
+    sales 	BIGINT
+);
+
+INSERT INTO Flipkart_tb (order_id, order_date, customer_name, sales ) VALUES ('1', '2023-01-01', 'Alexa', '1239 ');
+INSERT INTO Flipkart_tb (order_id, order_date, customer_name, sales ) VALUES ('2', '2023-01-02', 'Alexa', '1239 ');
+INSERT INTO Flipkart_tb (order_id, order_date, customer_name, sales ) VALUES ('3', '2023-01-03', 'Alexa', '1239 ');
+INSERT INTO Flipkart_tb (order_id, order_date, customer_name, sales ) VALUES ('4', '2023-01-03', 'Alexa', '1239 ');
+INSERT INTO Flipkart_tb (order_id, order_date, customer_name, sales ) VALUES ('5', '2023-01-01', 'Ramesh', '1239 ');
+INSERT INTO Flipkart_tb (order_id, order_date, customer_name, sales ) VALUES ('6', '2023-01-02', 'Ramesh', '1239 ');
+INSERT INTO Flipkart_tb (order_id, order_date, customer_name, sales ) VALUES ('7', '2023-01-03', 'Ramesh', '1239 ');
+INSERT INTO Flipkart_tb (order_id, order_date, customer_name, sales ) VALUES ('8', '2023-01-03', 'Neha', '1239');
+
+DROP TABLE Flipkart_Return
+CREATE TABLE Flipkart_Return 
+(
+    order_id	INT,
+    return_date 	DATE
+);
+
+INSERT INTO Flipkart_Return (order_id, return_date ) VALUES ('1', '2023-01-02 ');
+INSERT INTO Flipkart_Return (order_id, return_date ) VALUES ('2', '2023-01-04 ');
+INSERT INTO Flipkart_Return (order_id, return_date ) VALUES ('3', '2023-01-05 ');
+INSERT INTO Flipkart_Return (order_id, return_date ) VALUES ('7', '2023-01-06');
+
+
+----------------------------Solution 1---------------------------
+
+
+WITH CTE_A AS
+(
+SELECT Flipkart_tb.customer_name,  COUNT(Flipkart_tb.order_id) as 'Total Order',COUNT(Flipkart_Return.order_id) as 'Return Count'
+--CAST (COUNT(Flipkart_Return.order_id) AS float) / CAST ( NULLIF(COUNT(Flipkart_Return.order_id),0) AS float)
+
+FROM Flipkart_tb
+LEFT JOIN Flipkart_Return ON Flipkart_tb.order_id=Flipkart_Return.order_id
+GROUP BY Flipkart_tb.customer_name
+)
+
+SELECT customer_name,  CAST  (([Return Count]*1.0/[Total Order] * 1.0)*100 AS DECIMAL(4,2))      AS '% Of Return' FROM CTE_A
+WHERE CAST  (([Return Count]*1.0/[Total Order] * 1.0)*100 AS DECIMAL(4,2))>=75.00
+
+
+
+
+
+
+
+SELECT * 
+
+FROM Flipkart_tb
+LEFT JOIN Flipkart_Return ON Flipkart_tb.order_id=Flipkart_Return.order_id
+
+SELECT CONVERT ( FLOAT(25),2 )
